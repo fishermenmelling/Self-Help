f21ef9bd63632e6328cf64523f7c31760e1346f3
Mark Webster Melling <fishermenmelling@gmail.com>
2022-06-09 12:49:40 -0600

    Update Self Help.h

--- Self Help.h
+++ Self Help.h
@@ -1,151 +1,543 @@
 //
+//  See_what_happens.h
+//  See_what_happens
+//
 //  Created by mark melling on 5/24/22.
 //
 
+#ifndef See_what_happens_h
+#define See_what_happens_h
 #include <iostream>
-#include  <string>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+void foodMaking() {
+
+    std::cout << "How easy of food are you wanting to make?\n\n";
+    std::cout << "1 within the hour\n2 in need of a day's preparation\n\n";
+    std::cout << "Answer: ";
+    int b;
+    std::cin >> b;
+    std::cout << "\n";
+
+    switch (b) {
+        
+        case 1:
+            std::cout << "Chilli, potatoe, sour cream\n";
+            std::cout << "Stuffing\n";
+            std::cout << "Turkey Sandwitch\n";
+            std::cout << "Grilled tuna sandwiches\n";
+            std::cout << "Sub sandwich\n";
+            std::cout << "Grapes\n";
+            std::cout << "Bananas\n";
+            std::cout << "Protien Shake\n";
+            
+            break;
+            
+        case 2:
+            std::cout << "Hamburgers\n";
+            std::cout << "Shepards pie\n";
+            std::cout << "Crockpot chicken noodle soup\n";
+            std::cout << "Crockpot Chili\n";
+            std::cout << "Crockpot taco soup\n";
+            std::cout << "Crockpot bbq meatballs\n";
+            std::cout << "Burger Golosh\n";
+            
+            break;
+        
+        default:
+            break;
+    }
+}
+
 class ME {
     private:
-        int sin;
+        
         
     public:
-    int joy{0};
-        bool fear, on_covenant_path, patient, peace_of_mind;
-        
+    int repent{0}, holy, joy;
+    bool fear, on_covenant_path = true, patient, peace_of_mind, bad_behavior = false, poured_heart_out = true, prayed = true, easy_life = true, humble = true, faith_in_Jesus_Christ = true, hope = true, love = true, gratitude = true, grace = true;
+    
+    
 };
 
-/*void WaysToIgniteSpiritualMomentum(ME Me()) {
+
+std::vector<std::string> good_labels = {"Child of god", "Child of the covenant", "Desiple of Jesus Christ"};
+
+std::vector<std::string> how_to_get_miracles = {"We obey the prophet", "Consult the lord", "Expect miracles", "Faith && trust", "Trust leaders", "Look for miracles", "Ask for a miracle", "Start working towards it", "Action", "Be close to God’s presence", "Weight of the covenant", "Need the need for a miracle", "Keep your covenants && attend the temple", "Follow holy ghost's promptings"};
+
+std::vector<std::string> promises_to_house_of_Israel = {"fear not I know your sorrows", "I will not leave you", "I have come to deliver you", "Mine angels have charge over you", "I am in your midst", "I will do wonders among you", "Walk with me", "Learn of me", "I will give you rest", "You are mine"};
+
+std::vector<std::string> Secular_Ed_ignores = {"We will die", "We will be resurrected && immortal", "The judgement day will come"};
+
+std::vector<std::string> Heavenly_Father_Gave_Me = {"A nice home with great rent", "A nice car", "Great internet", "Scriptures && ability to pray", "Promptings to avoid unnecessary conflicts", "Super great job/position: w/ great pay, w/ good && nice people, w/ good hours"};
+
+std::vector<std::string> The_savior_imparts = {"hope I thought was lost forever", "strength I thought I could never posses", "healing I could never imagine was possible"};
+
+std::vector<std::string> gathering_Israel_is = {"helping someone MAKE covenant", "helping someone KEEP covenant", "Family history work", "missionary work", "very healing", "our covenant responsibility"};
+
+
+void FiveWaysToIgniteSpiritualMomentum(ME Me()) {
+    bool uplifting = Me().on_covenant_path, bad_behavior = false, media = false, easy_life = Me().easy_life, progress = uplifting;
     
+    
+    
+    std::vector<bool> life = {};
+
+    //my_life - Satan’s influence
+    life.push_back(media);
+    
+    if (Me().humble && Me().faith_in_Jesus_Christ) {
+       //he will empower me to repent through his grace;
+        if (progress == true) {
+            //Me().joy;
+        }
+    }
+    
+    ME Enos;
+    bool prayer = Enos.prayed, pour_out_heart = Enos.poured_heart_out;
+    bool finally_answered = prayer + pour_out_heart;
 }
 
-if (bored || sleepy) {
-    walk_outside
-}
-Else if (rainy || snowy) {
-    Treadmill;
-}
-Else if (paranoid) {
-    Stretch;
-}
+/*
+ 
+    //Discover the joy of daily repentance
+    
+    seek miracles + expect miracles + believe in Him == the lord will bless me with miracles;
+        //Miracles can take time
+ 
+ What lasts != generally come easy
 
- Promises to house of Israel
-    fear not I know your sorrows
-    I will not leave you
-    I have come to deliver you
-    Mine angels have charge over you
-    I am in your midst
-    I will do wonders among you
-    Walk with me
-    Learn of me
-    I will give you rest
-    You are mine
+ help of our Heavenly Father + Jesus Christ > all other helps
+ 
+ I need to make a move
+ !(sit idly && expect things to come to me)
+ 
+ ATONEMENT
+ 
+ (Do good BECAUSE of my salvation) && !(FOR my salvation)
 
+ !(I will head what the wicked may say)
 
+ Faith && focus on the lord Jesus Christ will fortify me with spiritual strength
 
+ The strength to cool down || making peace in contentions comes from my faith in Jesus Christ && my faith in his words.
+ 
+ Boundary lines != battle lines;
 
-ME Me();
+ !(one deserves to be beaten)
 
+ I serve + !(expect something in return)
 
-(understand an ordinance) == (write and read the scriptures);
+ Love god with: (all my heart) + (all my might) + (all my mind) + (all my strength) + (whole heartedly) + (without reservation) + (without compromise)
+ 
+ God loves me exactly this way now: (whole heartedly) + (without reservation) + (without compromise)
+ 
+ To feast == to savor
+ 
+ To feast > to taste
+ 
+ while (I feast upon the words of christ) {
+     the words are embedded upon the fleshy tables of my heart.
+ }
+ 
+ !(one can force me to change);
+ 
+ bool change_alert = (someone sends me a message) && (I choose what to do with that knowledge);
+ 
+ The Restoration is a process, !(an event) + it will continue until the lord comes again
+ for (int i = 0; i <= lord_comes_again; i++) {}
 
-//We have to learn to ponder (critically thinking)
+ 
 
-if (!(study to receive the spirit)) {
+GATHERING OF ISRAEL
+
+ In a good relationship the other person will !(want me to change) + !(I will want her to change)
+ 
+ strengths to me = weak things;
+ 
+ “I will do it myself” = false;
+  following him works = true;
+ 
+ 
+
+VAGUE LINES
+ 
+ Does perfectionism come from judging unrighteously?
+ 
+ Pure doctrine of christ
+ Pure revelation
+ Pure truth
+ 
+ !(judging others)
+ 
+ The holier you become the more you care about what...
+  
+ It is doctrinally incorrect to refer to the sacrifice as the atonement ||… (look up conference talk to find out how to refer to the atonement)
+
+ Raise awareness
+ “I will go && do?” vs. “How is it possible?”
+ No short cuts
+ We worship a waiting god
+ We must have a change of attitude
+  
+-------------------
+
+ Practice holiness continually
+ What would a holy young adult do?
+ //look up later what it means to be holy
+
+ !(honor labels > each other)
+  
+ 
+ 
+ When editing ALL OF THIS make sure to delete stuff that makes no sense
+ turn many lines into if statements && equations (+ && =)
+ see if "if statements" can be turned into equations
+ lists go to vectors
+ 
+ "WHILE" STATEMENTS
+ 
+ while (patience--) {
+      Me liable to make an error++;
+ }
+ 
+ While (I am more in awe with (Jesus Christ && his gospel)) {
+     Me.joy++;
+     I have more enthusiasm for God’s work++;
+     I recognize God’s hand in all things++;
+ }
+ 
+ while (I obey him) {
+    Jesus will make me more holy
+ }
+ 
+  while (I honor the conditions of the ordinances && covenants I make) {
+    I become closer to the lord++;
+   }
+ 
+ while (gathering_Israel) {
+   laborers < nonlaborers;
+ }
+
+   while (bringing the blessings of the temple to God’s faithful children) {
+     the lord gives me more insights to do it;
+   }
+
+  while (I do hard things with faith) {
+     trust = real;
+ }
+ 
+ -------------
+ 
+ "IF" STATEMENTS
+ 
+ fear != lead to grace
+ 
+ obligation != lead to grace
+ 
+ grace = motivates me to do good + gives me the ability to do good
+ 
+ I am saved by grace
+ 
+ if (Me().love && Me().gratitude) {
+    Me().grace;
+ }
+ 
+ if (make goal) {
+  make time for my goal to be reached;
+ }
+ else if (prompt to change way I am living the gospel) {
+     key to be able to follow the savior in a more stable way;
+ }
+ else if (I mess up){
+   Quickly repent;
+ }
+ else if (done my best) {
+     credit I deserve++;
+ }
+ else if (iPutInTime) {
+    the holy ghost will be there;
+ }
+ else if (I keep trying to end conflict in my personal life) {
+    Me(peace_of_mind) = true;
+    Me(fear) = false;
+ }
+ 
+ 
+ humility = having a broken heart + contrite spirit;
+ 
+ humility = The lord’s condition for change;
+ 
+ faith requires humility
+ 
+ faith = hope for things !(seen) which are true;
+ 
+ hope = sure + strong + fearless;
+ 
+ else if (people are being nasty towards me) {
+     People can still be loved
+ }
+ else if (someone points out someone else’s flaws) {
+  !(mean I should stop loving them as much)
+ }
+ else if (!(study to receive the spirit)) {
     bool pretend_to_be_following_it = false;
-}
-
-if (I_put_in_the_time) {
- the holy ghost will be there;
-}
-
-If (patience- -) {
-     I am liable to make an error;
-}
-
-if (prompt to change way I am living the gospel) {
-    think of it as a key to be able to follow the savior in a more stable way;
-}
-
-If (I_choose_righteous_choice) {
-    I have no need to fear;
-}
-
-If (the world hates me) {
+ }
+ else if (the world hates me) {
     Know that it hated Jesus before it hated me;
 }
+ 
+ -------------
+ 
+ PLAIN STATEMENTS
+ 
+ Understand 3 fundamental truths:
 
-tempting situations = sin;
+     -truth about who I am
+     -truth about what Jesus && Heavenly Father have offered to me
+     -truth related to my conversion
+ 
+ I need to be patiently striving towards a worthy cause
+ 
+ 
+ 
+ Heavenly Father gets his work done through his people && his people done by his work
+ 
+ He knows exactly what real terror && shame feel like && how it feels to be abandoned && broken
+ 
+ 
+ 
+ Remember certain things I was blessed with on that day
+ 
+ Asses my associates
 
-ME WaysToIgniteSpiritualMomentum(ME Me()) {
-    
-    bool *progress = Me(on_covenant_path);
-    int repent = Me(sin) - -;
-    if (progress == true) {
-        Me(joy)++; //and stay there
-    }
-    
-    
-    
-    
-    
-    
-        Repenting == (key to progress);
-        Me(on_covenant_path) != (mean life will be easy)
-        //Discover the joy of daily repentance
-            
-    
-    //Distinguishing the truths of god and the lies of satan is one of the challenges //of my day. Cast Satan’s influence out of my life. I need daily experiences            //worshiping the lord and learning his gospel
-    if(learning about God && how God works) {
-    
-    }
-    
-    if(seek miracles && expect miracles && believe in Him) {
-        //Miracles can take time.
-        the lord will bless me with miracles;
-    }
+ Find the balance between perfectionism && complacency
+ 
+ I need to RELY ON the atonement of Jesus Christ
+ My savior has power to right all wrongs
+ 
+ My savior has power to provide complete healing
+ 
+ Recognize that I have imperfections && correct them daily through the atonement
 
-    If (I keep trying to end conflict in my personal life) {
-        Me(peace_of_mind) = true;
-        Me(fear) = false;
-    }
+ A steady movement from “What do I want,” to “What does God want.”
+ 
+ Valiant saints enjoy the present && are satisfied
+ 
+ Invite the power of the Holy Ghost in their lives though meaningful scripture study proper prayer && proper preparation to participate
 
+ The ability to withhold from the big temptations of the adversary come from holding fast to the word of god && !(just clinging to it);
+ 
+ Give place in my heart for word_of_God
+
+ Be willing to let my will be swallowed up in the will of the father
+ 
+ Put all my energy && effort into ministering
+ 
+ What I get out of life = what I give
+ 
+ learn about God
+ learn about how God works
+ 
+ It's all about experimentation.
+ You gotta keep moving/experimenting + learning from results
+ 
+ Our ordinances act as a compass toward becoming closer to the savior
+ 
+ Sometimes (what worked in the past != working in the future)
+ 
+ In the end, it is the blessing of (a close && abiding relationship with the Father && the Son) that we seek
+
+ !(head what the wicked may say)
+
+ Jesus Christ gives simplicity in life
+ 
+ Ask the lord to teach me how to hear him better
+
+ Ask for him to put me to work
+ 
+ Find something to do to build my faith
+ 
+ Allow my affections to be consecrated for my gain
+ Let God prevail in my life
+
+ Watch for signs of depression
+ 
+ Immerse myself in revelation
+ 
+ Follow the holy ghost
+ 
+ We trust in the savior as our advocate
+ 
+ We trust in the savior as our mediator
+ 
+ We trust in the blessings of his infinite atonement
+ 
+ Education is very important
+ 
+ Learn from the past
+ Prepare for the future
+ Live in the present
+ 
+ Sharing the gospel brings joy && hope
+
+ Stand as a witness in ALL times && ALL places
+ 
+ Find cousins + find cousin’s cousins
+ 
+ Sequential order = a simple and natural way for the lord to teach us important principles.
+ 
+ Our growth is sequential.
+ 
+ Temple ordinances are in sequential order.
+ 
+ follow sequence the lord has outlined for us
+  
+ The ongoing restoration needs ongoing revelation
+
+ The first presidency requests guidance regularly in their decisions
+ 
+ We need to love everyone
+ 
+ Ask him for individual guidance
+
+ Come to him with any problem
+
+ Put Heavenly Father 1st always
+
+ We were adults in the premortality
+ 
+ I need daily experiences worshiping the lord && learning his gospel
+ 
+ Satan will confuse me to tempt me to choose the wrong
+ 
+ Julie Ashworth gave a good point about having a black && a white journal
+ Should I get a black journal && a white journal
+ 
+ know that most people want to help + are trying to help
+ 
+ I like to come back to what’s comfortable.
+ 
+ good_things come from God
+ 
+ I have lived && learned for eons of time.
+ As such I can study ponder && pray for the things I already knew
+ 
+ All good things come from god
+ 
+ Love of god needs to dwell in my heart
+ 
+ ponder = critically think;
+ 
+ I have to learn to ponder
+ --------------
+ 
+ "EQUALS" STATEMENTS
+ 
+ God’s love == the very power that motivates me to seek that tree of life
+ 
+ This life = the time to decide what our eternal life will be like
+
+ I talk to someone = I am helping build Zion
+  establishing good relationships
+ 
+ Life changing question = What would a holy young adult do?
+ 
+ Identifiers = powerful
+ 
+ labels = powerful
+ 
+ Many labels will change for me in passage of time
+ 
+ love others > labels
+ 
+ learn about the power of proximity (acting in place for another person)
+ 
+ amount pain I feel = view of where I sit;
+ 
+ Family history work = missionary work;
+ 
+if (!(building myself up)) {
+    I will break down;
 }
 
+ shrinking = !growing;
+ 
+ righteous_choice = !(need to fear);
+ 
+ 
+ 
+ if (Me.on_covenant_path + worthy) {
+ Increased sense of situation + Ability to sense danger;
+ 
+ }
+ 
+ lost people who walk in circles == probably !(know where straight ahead is)
+  
+ Without landmarks == I will end off course
+  
+ Looking down the road == a great way to drive straight
+ 
+ growth = willingness + natural curiosity + (level of faith + understanding);
+ 
+ studying scriptures + (have spirit of delightful discovery) + have faithful obedience to them == savoring the scriptures;
+ 
+ God = god of Justice + god of Mercy;
+ 
+ I make the covenants at the sacrament table == I will have him to always be with me
+ 
+ if (people have prayed for me) {
+    I need to pray for others
+ }
+ 
+ word_of_God = name for Jesus Christ;
 
+ word_of_God = Bible + BoM;
+ 
+ !(learn) == !(function);
 
+ if(listen_to_the_spirit) {
+    spirit tells me all things I should do;
+ }
+ 
+ grace = enabling power of the atonement;
+ 
+ Bringing others to him = how I become like Jesus Christ
+ 
+ std::string using_his_grace = "Reaching for devine help + seeking for devine help + getting Devine help";
+ 
+if (I use his grace) {
+    Ability to do good++;
+ }
+ 
+ tempting situations = sin;
 
+ Repenting = Me.on_covenant_path;
+ 
+ Repenting != easy_life
+ 
+ Distinguishing the truths of god from the lies of satan == one of the challenges of my day.
+ 
+ understand_an_ordinance = (read the scriptures) + (write about what I have just read);
+ 
+ The Ultimate Objective of gathering_Israel = bring the blessings of the temple to God’s faithful children;
+ 
+ if (Prayer + scripture_study) {
+    revelation++;
+ }
 
-If (I humble myself and have faith in Jesus Christ) {
-    he will empower me to change;
-    weak things can literally become strengths to me;
-}
-
-While (I am more in awe with Jesus Christ and his gospel) {
-    I become happier;
-    I have more enthusiasm for God’s work;
-    I recognize God’s hand in all things;
-}
-
-if (learn == 0) {
-    bool functioning = false;
-}
-
-if (listen_to_the_spirit) {
-    it tells me all things I should do;
-}
-
-if (done my best) {
-    credit I deserve++;
-}
-
-
-
-(Bringing others to him) == (how I become like Jesus Christ)
-
-*/
-
-
-#endif /* See_what_happens_h */
+ Life != competition
+ 
+ hypothesis:
+   if (I have negative emotions) {
+        hard for me to feel good emotions
+ }
+   assuming the worst of everything often comes from me being idle
+   assuming the worst of everything -> stress -> sensory overload -> autism
+   assuming the best of the everything -> makes me more likable
+ */
+ 
+#endif /* Self_Help_h */
